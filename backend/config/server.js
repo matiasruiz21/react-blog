7b32d4a8ed6424fcc79574794399fbc1334a6e7e
Matias Ruiz <matiasruiz6899@gmail.com>
2021-01-03 20:15:43 -0300

    Relocation

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '7b8f702dc14412aa71b847869037a4a0'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '79ff0cf5-f58a-4170-94d8-09b7e527f524'
-};
